4ee75b794653b7b2713017979259a84a29542e2f
its-cleb <148484251+its-cleb@users.noreply.github.com>
2024-02-15 11:16:21 -0800

    Main CSS Loaded
    - Added root styles to global.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google"
+import { Roboto } from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const roboto = Roboto({ weight: ['400', '500', '700', '900'], subsets: ["latin"] })
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <header>
           <p>Test</p>
         </header>
